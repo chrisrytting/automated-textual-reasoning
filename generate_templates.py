e9482a590f4f6a4a72febeab841b44fe0aad47fb
Chris Rytting <christophermichaelrytting@gmail.com>
2020-01-03 10:32:37 -0700

    Had wrong generate_templates.py

---
+++ generate_templates.py
@@ -0,0 +1,18 @@
+import random as random
+
+def 
+
+# Randomly generate two lists of integers which represent blocks 
+n = random.randint(0,20)
+list_1 = []
+list_2 = []
+for i in range(n):
+    random.choice((list_1, list_2)).append(i)
+print(list_1,list_2)
+
+# Generate a NL description of it
+description_1 = 
+
+# Perform random operations on that list, coming up with NL descriptions of those operations 
+
+# Generate a NL description of the manipulated list
